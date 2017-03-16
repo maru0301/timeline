011ce4673e9b1a7890c0e854cee8023c67c6bfbe
maru0301 <sp7p4z69@helen.ocn.ne.jp>
2017-03-16 19:03:10 +0900

    edit

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@
 // Global
 
 ///////////////////////////////////////
+/*
 var JSON_DATA_CHAMP_IMG = new Array();
 var JSON_DATA_TEAM = {};
 var JSON_DATA_SUMMONER_SPELL = new Array();
@@ -16,15 +17,19 @@ var VER_ITEM = "";
 var VER_MASTERY = "";
 
 var CDN_URL = "";
+*/
 
 ////////////////////////////////////////////////////////////////////////////////////
 // Error Message
-var ERROR_ID_VERSION_GET_ERROR 			= "バージョン情報が取得出来ませんでした";
+var ERROR_ID_VERSION_GET_ERROR 			= "サーバーバージョン情報が取得出来ませんでした";
+var ERROR_ID_REALM_GET_ERROR 			= "バージョン情報が取得出来ませんでした";
 var ERROR_ID_CHAMPION_IMG_GET_ERROR 	= "チャンピオンイメージ情報が取得出来ませんでした";
 var ERROR_ID_SUMMONER_SPELL_GET_ERROR 	= "チーム情報が取得出来ませんでした";
 var ERROR_ID_ITEM_IMG_GET_ERROR 		= "アイテムイメージ情報が取得出来ませんでした";
 var ERROR_ID_TEAM_GET_ERROR 			= "チーム情報が取得出来ませんでした";
 var ERROR_ID_MASTERY_IMG_GET_ERROR 		= "マスタリーイメージ情報が取得出来ませんでした";
+var ERROR_ID_MATCH_DETAILS_GET_ERROR	= "試合情報が取得出来ませんでした";
+var ERROR_ID_MATCH_TIMELINE_GET_ERROR	= "タイムライン情報が取得出来ませんでした";
 
 /////////////////////////////////////////////////
 //
@@ -37,122 +42,392 @@ function errorDlg(msg)
 ////////////////////////////////////////////////////////////////////////////////////
 //
 
-var data = location.href.split("?")[1];
-var text = data.split("=")[1];
-var matchdetail_url = decodeURIComponent(text);
-
-
-var request = [
-	{ error_id: ERROR_ID_VERSION_GET_ERROR,			url: './php/main.php', data: { func:"GetVersion" },  }, // Version
-	{ error_id: ERROR_ID_CHAMPION_IMG_GET_ERROR,	url: './php/main.php', data: { func:"GetChampionImage" },  },
-	{ error_id: ERROR_ID_SUMMONER_SPELL_GET_ERROR,	url: './php/main.php', data: { func:"GetSummonerSpells" },  },
-	{ error_id: ERROR_ID_ITEM_IMG_GET_ERROR,		url: './php/main.php', data: { func:"GetItem" },  },
-	{ error_id: ERROR_ID_TEAM_GET_ERROR,			url: './json/team.json', data: {},  },
-	{ error_id: ERROR_ID_MASTERY_IMG_GET_ERROR,		url: './php/main.php', data: { func:"GetMasteryImage"},  },
-];
-
-var jqXHRList = [];
-
-for( var i = 0, max = request.length ; i < max ; ++i )
+function main()
 {
-	jqXHRList.push($.ajax(
+	var data = location.href.split("?")[1];
+	var text = data.split("=")[1];
+	var url = decodeURIComponent(text);
+
+	var index = url.search("#");
+	url = url.substr(index);
+	index = url.search("/");
+	url = url.substr(index+1);
+	index = url.search("/");
+
+	var gameRealm = url.substr(0, index);
+
+	url = url.substr(index+1);
+	index = url.search('[\?]');
+
+	var gameId = url.substr(0, index);
+
+	url = url.substr(index+1);
+	index = url.search('=');
+	url = url.substr(index+1);
+	index = url.search('&');
+
+	if( index != -1 )
+		url = url.substr(0, index);
+
+	var gameHash = url;
+
+	var request = [
+		{ error_id: ERROR_ID_MATCH_DETAILS_GET_ERROR,	url: './php/main.php', data: { func:"GetMatchDetails", realm:gameRealm, id:gameId, hash:gameHash },  },
+		{ error_id: ERROR_ID_MATCH_TIMELINE_GET_ERROR,	url: './php/main.php', data: { func:"GetMatchTimeline", realm:gameRealm, id:gameId, hash:gameHash },  },
+		{ error_id: ERROR_ID_VERSION_GET_ERROR,			url: './php/main.php', data: { func:"GetVersion" },  },
+	];
+
+	var jqXHRList = [];
+
+	for( var i = 0, max = request.length ; i < max ; ++i )
 	{
-		url: request[i].url,
+		jqXHRList.push($.ajax(
+		{
+			url: request[i].url,
+			type: 'GET',
+			dataType: 'json',
+			data: request[i].data,
+		}));
+	}
+
+	$.when.apply(null, jqXHRList).done(function ()
+	{
+		console.log("Success : Main");
+
+		var json = [];
+		var statuses = [];
+		var jqXHRResultList = [];
+		
+		for( var i = 0, max = arguments.length ; i < max ; ++i )
+		{
+			var result = arguments[i];
+			json.push(result[0]);
+			statuses.push(result[1]);
+			jqXHRResultList.push(result[3]);
+		}
+
+		var matchDetailJson = json[0];
+		var matchTimelneJson = json[1];
+		var versionJson = json[2];
+
+		var matchDetailData = { game:{}, teams:[] };
+		matchDetailData.game = GetMatchData(matchDetailJson);
+		matchDetailData.teams = GetTeamData(matchDetailJson);
+
+		console.log("------- json -------");
+		console.log(json);
+		console.log("------- matchDetailData -------");
+		console.log(matchDetailData);
+
+		var version = GetVersion(matchDetailData.game.gameVer, versionJson);
+		TimeLineMain(version, matchDetailData);
+
+	});
+
+	$.when.apply(null, jqXHRList).fail(function ()
+	{
+		console.log("Fail : Main");
+		console.log(jqXHRList);
+
+		for( var i = 0 ; i < jqXHRList.length ; ++i )
+		{
+			if( jqXHRList[i].statusText === "error" )
+			{
+				errorDlg(request[i].error_id);
+			}
+		}
+	});
+/*
+	$.ajax(
+	{
+		url: './php/main.php',
 		type: 'GET',
 		dataType: 'json',
-		data: request[i].data,
-	}));
+		scriptCharset: 'utf-8',
+		data: { func:"GetMatchDetails", realm:gameRealm, id:gameId, hash:gameHash },
+		
+		success: function (data)
+		{
+			console.log("GetURL : Success");
+			console.log(data);
+
+			MATCH_HISTORY_JSON.game = {};
+
+			MATCH_HISTORY_JSON.game = GetMatchData(data);
+			MATCH_HISTORY_JSON.teams = [];
+			MATCH_HISTORY_JSON.teams = GetTeamData(data);
+
+			console.log(MATCH_HISTORY_JSON);
+			
+			var version = MATCH_HISTORY_JSON.game.gameVer;
+			var index = version.search("[\.]");
+			var gameVersion = version.substr(0,index+1);
+			version = version.substr(index+1);
+			index = version.search("[\.]");
+			gameVersion = gameVersion + version.substr(0,index+2);
+
+			var request = [
+				{ error_id: ERROR_ID_VERSION_GET_ERROR,			url: './php/main.php', data: { func:"GetVersion" },  },
+				{ error_id: ERROR_ID_CHAMPION_IMG_GET_ERROR,	url: './php/main.php', data: { func:"GetChampionImage", ver:gameVersion },  },
+//				{ error_id: ERROR_ID_SUMMONER_SPELL_GET_ERROR,	url: './php/main.php', data: { func:"GetSummonerSpells", ver:gameVersion },  },
+//				{ error_id: ERROR_ID_ITEM_IMG_GET_ERROR,		url: './php/main.php', data: { func:"GetItem", ver:gameVersion },  },
+//				{ error_id: ERROR_ID_MASTERY_IMG_GET_ERROR,		url: './php/main.php', data: { func:"GetMasteryImage", ver:gameVersion },  },
+			//	{ error_id: ERROR_ID_TEAM_GET_ERROR,			url: './json/team.json', data: {},  },
+			//	{ error_id: ERROR_ID_MATCH_DETAILS_GET_ERROR,	url: './php/main.php', data: { func:"GetMatchDetails", realm:gameRealm, id:gameId, hash:gameHash },  },
+			];
+
+			var jqXHRList = [];
+
+			for( var i = 0, max = request.length ; i < max ; ++i )
+			{
+				jqXHRList.push($.ajax(
+				{
+					url: request[i].url,
+					type: 'GET',
+					dataType: 'json',
+					data: request[i].data,
+				}));
+			}
+
+			$.when.apply(null, jqXHRList).done(function ()
+			{
+				var json = [];
+				var statuses = [];
+				var jqXHRResultList = [];
+				
+				for( var i = 0, max = arguments.length ; i < max ; ++i )
+				{
+					var result = arguments[i];
+					json.push(result[0]);
+					statuses.push(result[1]);
+					jqXHRResultList.push(result[3]);
+				}
+				
+				///////////////////////////////////////////////////////////
+				// Global情報取得
+				///////////////////////////////////////////////////////////
+				var verJson = json[0];
+				var champImgJson = json[1];
+				var spellJson = json[2];
+				var itemImgJson = json[3];
+				var masteryImgJson = json[4];
+			//	JSON_DATA_TEAM = json[4];
+//				var matchdetailJson = json[5];
+				
+				// ソート
+				for(var key in champImgJson.data)
+					JSON_DATA_CHAMP_IMG.push(champImgJson.data[key]);
+				
+				JSON_DATA_CHAMP_IMG.sort(function(a, b)
+					{
+						if(a.key < b.key) return -1;
+						if(a.key > b.key) return 1;
+						if(a.key == b.key) return 0;
+					}
+				);
+				
+				for(var key in itemImgJson.data)
+					JSON_DATA_ITEM[key] = itemImgJson.data[key];
+				
+				for(var key in spellJson.data)
+				{
+					var id = spellJson.data[key].id;
+					JSON_DATA_SUMMONER_SPELL[id] = spellJson.data[key];
+				}
+
+				JSON_DATA_SUMMONER_SPELL.sort(function(a, b)
+					{
+						if(a.name < b.name) return -1;
+						if(a.name > b.name) return 1;
+						if(a.name == b.name) return 0;
+					}
+				);
+				
+				for(var key in masteryImgJson.data)
+				{
+					JSON_DATA_MASTERY_IMG[key] = masteryImgJson.data[key];
+				}
+				JSON_DATA_MASTERY_IMG.tree = masteryImgJson.tree;
+				
+				JSON_DATA_MASTERY_IMG.sort(function(a, b)
+					{
+						if(a.name < b.name) return -1;
+						if(a.name > b.name) return 1;
+						if(a.name == b.name) return 0;
+					}
+				);
+				
+				// Version
+				VER_CHAMPION = verJson.n.champion;
+				VER_SN_SPELLS = verJson.n.summoner;
+				VER_ITEM = verJson.n.item;
+				VER_MASTERY = verJson.n.mastery;
+
+				CDN_URL = verJson.cdn;
+
+				ReworkJson();
+			});
+
+			$.when.apply(null, jqXHRList).fail(function ()
+			{
+				for( var i = 0 ; i < jqXHRList.length ; ++i )
+				{
+					if( jqXHRList[i].statusText === "error" )
+					{
+						errorDlg(request[i].error_id);
+					}
+				}
+			});
+		},
+
+		error: function (XMLHttpRequest, textStatus, errorThrown)
+		{
+			console.log("GetURL : Fail");
+			console.log(XMLHttpRequest.responseText);
+			console.log(textStatus);
+			console.log(errorThrown);
+			errorDlg(ERROR_ID_MATCH_DETAILS_GET_ERROR);
+		}
+	});
+*/
 }
 
-$.when.apply(null, jqXHRList).done(function ()
+function TimeLineMain(version, matchDetailData)
 {
-	var json = [];
-	var statuses = [];
-	var jqXHRResultList = [];
-	
-	for( var i = 0, max = arguments.length ; i < max ; ++i )
+	var request = [
+		{ error_id: ERROR_ID_REALM_GET_ERROR,			url: './php/main.php', data: { func:"GetRealm" },  },
+		{ error_id: ERROR_ID_CHAMPION_IMG_GET_ERROR,	url: './php/main.php', data: { func:"GetChampionImage", ver:version },  },
+		{ error_id: ERROR_ID_SUMMONER_SPELL_GET_ERROR,	url: './php/main.php', data: { func:"GetSummonerSpells", ver:version },  },
+		{ error_id: ERROR_ID_ITEM_IMG_GET_ERROR,		url: './php/main.php', data: { func:"GetItem", ver:version },  },
+		{ error_id: ERROR_ID_MASTERY_IMG_GET_ERROR,		url: './php/main.php', data: { func:"GetMasteryImage", ver:version },  },
+//	{ error_id: ERROR_ID_TEAM_GET_ERROR,			url: './json/team.json', data: {},  },
+//	{ error_id: ERROR_ID_MATCH_DETAILS_GET_ERROR,	url: './php/main.php', data: { func:"GetMatchDetails", realm:gameRealm, id:gameId, hash:gameHash },  },
+	];
+
+	var jqXHRList = [];
+
+	for( var i = 0, max = request.length ; i < max ; ++i )
 	{
-		var result = arguments[i];
-		json.push(result[0]);
-		statuses.push(result[1]);
-		jqXHRResultList.push(result[3]);
-	}
-	
-	///////////////////////////////////////////////////////////
-	// Global情報取得
-	///////////////////////////////////////////////////////////
-	var verJson = json[0];
-	var champImgJson = json[1];
-	var spellJson = json[2];
-	var itemImgJson = json[3];
-	var masteryImgJson = json[5];
-	
-	JSON_DATA_TEAM = json[4];
-	
-	// ソート
-	for(var key in champImgJson.data)
-		JSON_DATA_CHAMP_IMG.push(champImgJson.data[key]);
-	
-	JSON_DATA_CHAMP_IMG.sort(function(a, b)
+		jqXHRList.push($.ajax(
 		{
-			if(a.key < b.key) return -1;
-			if(a.key > b.key) return 1;
-			if(a.key == b.key) return 0;
-		}
-	);
-	
-	for(var key in itemImgJson.data)
-		JSON_DATA_ITEM[key] = itemImgJson.data[key];
-	
-	for(var key in spellJson.data)
-	{
-		var id = spellJson.data[key].id;
-		JSON_DATA_SUMMONER_SPELL[id] = spellJson.data[key];
+			url: request[i].url,
+			type: 'GET',
+			dataType: 'json',
+			data: request[i].data,
+		}));
 	}
 
-	JSON_DATA_SUMMONER_SPELL.sort(function(a, b)
-		{
-			if(a.name < b.name) return -1;
-			if(a.name > b.name) return 1;
-			if(a.name == b.name) return 0;
-		}
-	);
-	
-	for(var key in masteryImgJson.data)
+	$.when.apply(null, jqXHRList).done(function ()
 	{
-		JSON_DATA_MASTERY_IMG[key] = masteryImgJson.data[key];
-	}
-	JSON_DATA_MASTERY_IMG.tree = masteryImgJson.tree;
-	
-	JSON_DATA_MASTERY_IMG.sort(function(a, b)
+		console.log("Success : TimeLine");
+
+		var json = [];
+		var statuses = [];
+		var jqXHRResultList = [];
+		
+		for( var i = 0, max = arguments.length ; i < max ; ++i )
 		{
-			if(a.name < b.name) return -1;
-			if(a.name > b.name) return 1;
-			if(a.name == b.name) return 0;
+			var result = arguments[i];
+			json.push(result[0]);
+			statuses.push(result[1]);
+			jqXHRResultList.push(result[3]);
 		}
-	);
-	
-	// Version
-	VER_CHAMPION = verJson.n.champion;
-	VER_SN_SPELLS = verJson.n.summoner;
-	VER_ITEM = verJson.n.item;
-	VER_MASTERY = verJson.n.mastery;
-	
-	CDN_URL = verJson.cdn;
+		
+		console.log("------- json -------");
+		console.log(json);
 
-	ReworkJson();
-});
+		var realmJson = json[0];
+		var champImgJson = json[1];
+		var spellJson = json[2];
+		var itemImgJson = json[3];
+		var masteryImgJson = json[4];
+	//	JSON_DATA_TEAM = json[4];
+//				var matchdetailJson = json[5];
 
-$.when.apply(null, jqXHRList).fail(function ()
-{
-	for( var i = 0 ; i < jqXHRList.length ; ++i )
+		var championImgData = new Array();
+		var spellImgData = new Array();
+		var itemImgImgData = new Array();
+		var masteryImgData = new Array();
+
+		// ソート
+		for(var key in champImgJson.data)
+			championImgData.push(champImgJson.data[key]);
+		
+		championImgData.sort(function(a, b)
+			{
+				if(a.key < b.key) return -1;
+				if(a.key > b.key) return 1;
+				if(a.key == b.key) return 0;
+			}
+		);
+		
+		for(var key in itemImgJson.data)
+			itemImgImgData[key] = itemImgJson.data[key];
+		
+		for(var key in spellJson.data)
+		{
+			var id = spellJson.data[key].id;
+			spellImgData[id] = spellJson.data[key];
+		}
+
+		spellImgData.sort(function(a, b)
+			{
+				if(a.name < b.name) return -1;
+				if(a.name > b.name) return 1;
+				if(a.name == b.name) return 0;
+			}
+		);
+		
+		for(var key in masteryImgJson.data)
+		{
+			masteryImgData[key] = masteryImgJson.data[key];
+		}
+
+		masteryImgData.tree = masteryImgJson.tree;
+		
+		masteryImgData.sort(function(a, b)
+			{
+				if(a.name < b.name) return -1;
+				if(a.name > b.name) return 1;
+				if(a.name == b.name) return 0;
+			}
+		);
+		
+		// Version
+		/*
+		VER_CHAMPION = verJson.n.champion;
+		VER_SN_SPELLS = verJson.n.summoner;
+		VER_ITEM = verJson.n.item;
+		VER_MASTERY = verJson.n.mastery;
+
+		CDN_URL = verJson.cdn;
+		*/
+		var cdn_url = realmJson.cdn;
+
+//		ReworkJson();
+		console.log("------- championImgData -------");
+		console.log(championImgData);
+		console.log("------- spellImgData -------");
+		console.log(spellImgData);
+		console.log("------- itemImgImgData -------");
+		console.log(itemImgImgData);
+		console.log("------- masteryImgData -------");
+		console.log(masteryImgData);
+
+		ShowTimeLineCanvas(cdn_url, version);
+	});
+
+	$.when.apply(null, jqXHRList).fail(function ()
 	{
-		if( jqXHRList[i].statusText === "error" )
+		for( var i = 0 ; i < jqXHRList.length ; ++i )
 		{
-			errorDlg(request[i].error_id);
+			if( jqXHRList[i].statusText === "error" )
+			{
+				errorDlg(request[i].error_id);
+			}
 		}
-	}
-});
+	});
+}
 
 ////////////////////////////////////////////////////////////////////////////////////
 
@@ -428,6 +703,154 @@ function ShowMasteryIcon(data, key, getName, createName)
 	target.appendChild(newTag);
 }
 
+function ShowTimeLineCanvas(cdn_url, version)
+{
+	var target = document.getElementById("timeline_canvas");
+	var ctx = target.getContext('2d');
+	var champ_img = "Aatrox.png";
+
+	var imgData = new Array();
+	imgData.push( {src:"./img/minimap.png", img:new Image() });
+
+	for( var i = 0 ; i < 1 ; ++i )
+	{
+		imgData.push(
+			{
+				src:cdn_url + "/" + version + "/img/champion/" + champ_img,
+				img:new Image(),
+				reSize:[64,64],
+		});
+	}
+
+	var srcImg = [
+		"./img/minimap.png",
+		cdn_url + "/" + version + "/img/champion/" + champ_img,
+		cdn_url + "/" + version + "/img/champion/" + champ_img,
+	];
+
+	var img = [];
+	for(var i = 0 ; i < imgData.length ; ++i)
+	{
+//		img[i] = new Image();
+//		img[i].src = srcImg[i];
+		imgData[i].img.src = imgData[i].src;
+	}
+
+    var loadedCount = 1;
+	for(var i = 0 ; i < imgData.length ; ++i)
+	{
+		imgData[i].img.addEventListener('load', function()
+		{
+			if(loadedCount == imgData.length)
+			{
+				var x = 0;
+				var y = 0;
+				for (var j = 0 ; j < imgData.length ; ++j)
+				{
+					if( j == 0 )
+					{
+						target.width = imgData[j].img.width;
+						target.height = imgData[j].img.height;
+					}
+					if(imgData[j].reSize == undefined)
+						ctx.drawImage(imgData[j].img, x, y);
+					else
+						ctx.drawImage(imgData[j].img, 0, 0, imgData[j].img.width, imgData[j].img.height, 0, 100, 64, 64);					
+//						ctx.drawImage(imgData[j].img, x, y, imgData[j].img.width, imgData[j].img.height, 0, 0, imgData[j].reSize[0], imgData[j].reSize[1]);
+					
+//					x += 50;
+					y -= 100;
+				}
+			}
+			loadedCount++;
+		}, false);
+	}
+	/*
+	for(var i = 0 ; i < srcImg.length ; ++i)
+	{
+		img[i].addEventListener('load', function()
+		{
+			if (loadedCount == img.length)
+			{
+				var x = 0;
+				var y = 0;
+				for (var j in img)
+				{
+					if( j == 0 )
+					{
+						target.width = img[j].width;
+						target.height = img[j].height;
+					}
+					ctx.drawImage(img[j], x, y);
+					x += 50;
+					y += 70;
+				}
+			}
+			loadedCount++;
+		}, false);
+	}
+	*/
+
+	/*
+	var img = new Image();
+	img.src = "./img/minimap.png";
+
+	var champ_img = "Aatrox.png"
+
+	img.src = cdn_url + "/" + version + "/img/champion/" + champ_img;
+//	img.src = "./img/minimap.png";
+	img.addEventListener('load', function() {
+		ctx.clearRect(0,0, img.width, img.height);
+		ctx.drawImage(img, 0, 0, img.width, img.height, 0, 0, 64, 64 );
+}, false);
+
+	img.addEventListener('load', function() {
+		target.width = img.width;
+		target.height = img.height;
+		ctx.clearRect(0,0, img.width, img.height);
+		ctx.drawImage(img, 0, 0);
+	}, false);
+	*/
+}
+
+function ShowChampionCanvas(cdn_url, version)
+{
+	/*
+	var target = document.getElementById("canvas");
+	var newTag;
+
+		newTag = document.createElement("canvas");
+		newTag.id = "blue_1_champion_canvas";
+		newTag.width = 64;
+		newTag.height = 64;
+
+		target.appendChild(newTag);
+		
+		var ctx = newTag.getContext('2d');
+		var img = new Image();
+		var champ_img = "Aatrox.png"
+
+		img.src = cdn_url + "/" + version + "/img/champion/" + champ_img;
+		img.addEventListener('load', function() {
+			ctx.clearRect(0,0, img.width, img.height);
+			ctx.drawImage(img, 0, 0, img.width, img.height, 0, 0, 64, 64 );
+	}, false);
+	*/
+	
+	var target = document.getElementById("canvas_map");
+
+	var ctx = target.getContext('2d');
+	var img = new Image();
+	var champ_img = "Aatrox.png"
+
+	img.src = cdn_url + "/" + version + "/img/champion/" + champ_img;
+//	img.src = "./img/minimap.png";
+	img.addEventListener('load', function() {
+		ctx.clearRect(0,0, img.width, img.height);
+		ctx.drawImage(img, 0, 0, img.width, img.height, 0, 0, 64, 64 );
+}, false);
+}
+
 ////////////////////////////////////////////////////////////////////////////////////
 
 function GetItemMasteryName(data,id)
@@ -447,6 +870,28 @@ function GetItemMasteryName(data,id)
 	}
 	
 	return name;
+}
+
+function GoldTok(num)
+{
+	num /= 100; 
+	num = Math.round(num);
+	num /= 10;
+
+	return num;
+}
+
+function GetVersion(ver, json)
+{
+	var num = json.length;
+
+	while(--num)
+	{
+		if(ver.indexOf(json[num]) !== -1)
+		{
+			return json[num];
+		}
+	}
 }
 
 ////////////////////////////////////////////////////////////////////////////////////
@@ -622,11 +1067,7 @@ function GetPlayerData(data, teamId)
 	return set_data;
 }
 
-function GoldTok(num)
-{
-	num /= 100; 
-	num = Math.round(num);
-	num /= 10;
+////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////
 
-	return num;
-}
+main();
--- php/main.php
+++ php/main.php
@@ -25,7 +25,7 @@ class RiotApi
 		return $json;
 	}
 	
-	public function GetVersion()
+	public function GetRealm()
 	{
 		$json = $this->GetJson('https://global.api.pvp.net/api/lol/static-data/jp/v1.2/realm?api_key=');
 		
@@ -34,28 +34,85 @@ class RiotApi
 	
 	public function GetChampionImage()
 	{
-		$json = $this->GetJson('https://global.api.pvp.net/api/lol/static-data/jp/v1.2/champion?champData=image&api_key=');
+		$version = $_GET['ver'];
+		$json = $this->GetJson('https://global.api.pvp.net/api/lol/static-data/jp/v1.2/champion?version=' . $version . '&champData=image&api_key=');
 		
 		return $json;
 	}
 	
 	public function GetSummonerSpells()
 	{
-		$json = $this->GetJson('https://global.api.pvp.net/api/lol/static-data/na/v1.2/summoner-spell?spellData=image&api_key=');
+		$version = $_GET['ver'];
+		$json = $this->GetJson('https://global.api.pvp.net/api/lol/static-data/na/v1.2/summoner-spell?version=' . $version . '&spellData=image&api_key=');
 		
 		return $json;
 	}
 	
 	public function GetItem()
 	{
-		$json = $this->GetJson('https://global.api.pvp.net/api/lol/static-data/na/v1.2/item?itemListData=from,image,tags&api_key=');
+		$version = $_GET['ver'];
+		$json = $this->GetJson('https://global.api.pvp.net/api/lol/static-data/na/v1.2/item?version=' . $version . '&itemListData=from,image,tags&api_key=');
 		
 		return $json;
 	}
 	
 	public function GetMasteryImage()
 	{
-		$json = $this->GetJson('https://global.api.pvp.net/api/lol/static-data/na/v1.2/mastery?masteryListData=image,ranks,tree&api_key=');
+		$version = $_GET['ver'];
+		$json = $this->GetJson('https://global.api.pvp.net/api/lol/static-data/na/v1.2/mastery?version=' . $version . '&masteryListData=image,ranks,tree&api_key=');
+		
+		return $json;
+	}
+
+	public function GetMatchDetails()
+	{
+		$gameRealm = $_GET['realm'];
+		$gameId = $_GET['id'];
+		$gameHash = $_GET['hash'];
+
+		$proxy = array(
+			"http" => array(
+					"proxy" => "proxy2.hq.scei.sony.co.jp:10080",
+					'request_fulluri' => true,
+				),
+		);
+		
+		$proxy_context = stream_context_create($proxy);
+
+		$url = "https://acs.leagueoflegends.com/v1/stats/game/" . $gameRealm . "/" . $gameId . "?gameHash=" . $gameHash;
+
+		$json = file_get_contents($url, false, $proxy_context);
+		$json = mb_convert_encoding($json, 'UTF8', 'ASCII,JIS,UTF-8,EUC-JP,SJIS-WIN');
+		
+		return $json;
+	}
+
+	public function GetMatchTimeline()
+	{
+		$gameRealm = $_GET['realm'];
+		$gameId = $_GET['id'];
+		$gameHash = $_GET['hash'];
+
+		$proxy = array(
+			"http" => array(
+					"proxy" => "proxy2.hq.scei.sony.co.jp:10080",
+					'request_fulluri' => true,
+				),
+		);
+		
+		$proxy_context = stream_context_create($proxy);
+
+		$url = "https://acs.leagueoflegends.com/v1/stats/game/" . $gameRealm . "/" . $gameId . "/timeline?gameHash=" . $gameHash;
+
+		$json = file_get_contents($url, false, $proxy_context);
+		$json = mb_convert_encoding($json, 'UTF8', 'ASCII,JIS,UTF-8,EUC-JP,SJIS-WIN');
+		
+		return $json;
+	}
+	
+	public function GetVersion()
+	{
+		$json = $this->GetJson('https://global.api.pvp.net/api/lol/static-data/JP/v1.2/versions?api_key=');
 		
 		return $json;
 	}
@@ -66,11 +123,14 @@ class RiotApi
 $api = new RiotApi;
 
 $func_tbl = array(
-			"GetVersion" => "GetVersion",
+			"GetRealm" => "GetRealm",
 			"GetChampionImage" => "GetChampionImage",
 			"GetSummonerSpells" => "GetSummonerSpells",
 			"GetItem" => "GetItem",
 			"GetMasteryImage" => "GetMasteryImage",
+			"GetMatchDetails" => "GetMatchDetails",
+			"GetMatchTimeline" => "GetMatchTimeline",
+			"GetVersion" => "GetVersion",
 );
 
 //-------------------------------------------------
